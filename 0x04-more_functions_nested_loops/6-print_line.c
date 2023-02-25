acaae1cdb4c4b9f9fc2564314f5823d3008315bb
RxRhema <Vincent.rhema@gmail.com>
2023-02-24 18:23:44 -0800

    nested

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,38 +2,27 @@
 
 
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
- */
+* print_line - draws a straight line in the terminal
+* @n: number of times the character _ should be printed
+* Return: empty
+*/
 
 void print_line(int n)
-
 {
+
+int x;
 
 if (n <= 0)
-
 {
-
 _putchar('\n');
-
+}
 else
-
 {
-
-int i;
-
-
-for (i = 1; i <= n; i++)
-
+for (x = 0; x <= n; x++)
 {
-
-_putchar('_');
-
+_putchar(95);
 }
-
 _putchar('\n');
-
 }
-
 
 }
