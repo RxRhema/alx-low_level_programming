a08db1574c58273db4362d7472fc285c6e5b9f3b
RxRhema <Vincent.rhema@gmail.com>
2023-02-24 20:30:08 -0800

    m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,15 +2,14 @@
 
 
 /**
-* print_diagonal - draws a diagonal line on the terminal
-* @n: number of times the character \ should be printed
+* print_diagonal - Draws a diagonal lines according parameter
+* @n: The number of times to print diagonal lines
 *Return empty
 */
 
 void print_diagonal(int n)
 
 {
-
 
 int x, y;
 if (n <= 0)
@@ -20,14 +19,14 @@ _putchar('\n');
 else
 {
 for (x = 0; x < n; x++)
-{
+}
 for (y = 0; y < x; y++)
 {
 _putchar(32);
 }
 _putchar(92);
+{
 _putchar('\n');
 }
-
 
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,34 +6,24 @@
 * @size: size of the square
 */
 void print_square(int size)
-
-{
-if (size <= 0)
-}
-_putchar('\n');
-
-else
 {
 
 int i, j;
 
-
-for (i = 0; i < size; i++)
-
+if (size <= 0)
 {
-
-for (j = 0; j < size; j++)
-
-{
-
-_putchar('#');
-
-}
-
 _putchar('\n');
-
 }
-
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
 }
 
 }
